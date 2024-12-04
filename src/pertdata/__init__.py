43129f414b8cd22a1fcb39b6aa88b0175b393624
Jan Voges <janvoges.git@icloud.com>
2024-12-04 19:11:12 +0100

    Remove __cache_dir_path__ from public API

--- src/pertdata/__init__.py
+++ src/pertdata/__init__.py
@@ -1,8 +1,7 @@
 """Public API for the pertdata package."""
 
 from .pert_dataset import PertDataset
-from .utils import cache_dir_path, datasets
+from .utils import datasets
 from .version import __version__
 
 __all__ = ["PertDataset", "datasets", __version__]
-__cache_dir_path__ = cache_dir_path()
