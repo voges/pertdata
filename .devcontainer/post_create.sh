e38b69677182f95a207a3d4d5421b60b3c2c76a1
Jan Voges <janvoges.git@icloud.com>
2025-01-27 15:28:09 +0100

    Update post_create.sh

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -3,9 +3,11 @@
 # Remove all editor settings for Python from the VS Code remote settings.
 sed --in-place '/"\[python\]": {/,/}/d' /home/vscode/.vscode-server/data/Machine/settings.json
 
+# Install the Python dependencies.
+pip --disable-pip-version-check install --requirement requirements.txt
+
 # Install additional packages.
 sudo apt-get update && sudo apt-get install --yes shellcheck
 
-# Install the pertdata package.
-pip --disable-pip-version-check install --requirement requirements.txt
+# Install pertdata in editable mode.
 pip install --editable .
