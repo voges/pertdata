3520001c6c81f3918efd76c6591d8ee20faa50ba
Jan Voges <janvoges.git@icloud.com>
2025-02-11 15:43:10 +0100

    Update __all__ in __init__.py

--- src/pertdata/__init__.py
+++ src/pertdata/__init__.py
@@ -4,4 +4,4 @@ from .pert_dataset import PertDataset
 from .utils import datasets
 from .version import __version__
 
-__all__ = ["PertDataset", "datasets", __version__]
+__all__ = [PertDataset, datasets, __version__]
